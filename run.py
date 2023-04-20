9e5a3a867aed478fff03b6c1ed21719502d20404
DRegan-dev <damienreganmusic@gmail.com>
2023-04-20 23:45:45 +0000

    add if statment to handle guess that are outside the bounds of the board

--- run.py
+++ run.py
@@ -29,3 +29,6 @@ for turn in range(6):
     guess_row = int(input("Guess Row(O-" + str(size -1) + "): ))
     guess_col = int(input("Guess Col(O-" + str(size -1) + "): ))
 
+    if guess_row < 0 or guess_row >= size or guess_col < 0 or guess_col >= size:
+        print("Oops, you hit land")
+
