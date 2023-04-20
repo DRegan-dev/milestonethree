21c6ea048b60c5ee338ac616ece64619a22087b2
DRegan-dev <damienreganmusic@gmail.com>
2023-04-20 23:16:13 +0000

    add function that creates two dimensional list that represents the battleship game board

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 from random import randint
+
+def create_board(b_size):
+    """
+    Creates two dimensional list that represents the game board of the battleship game.
+    """
+    board = []
+    for i in range(b_size):
+        board.append([0] * b_size)
+    return board
