fd2570c3174425f74d456fbc03e4ea8f33f0249f
DRegan-dev <damienreganmusic@gmail.com>
2023-04-20 23:20:19 +0000

    define function that prints board to terminal

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ def create_board(b_size):
     for i in range(b_size):
         board.append([0] * b_size)
     return board
+
+def print_board(board):
+    """
+    Prints game board to the terminal
+    """
+    for row in board:
+        print(" ".join(row))
