dfefb32e99ee1002bac3bf3fcbaa93ef01ad70cc
DRegan-dev <damienreganmusic@gmail.com>
2023-04-21 01:43:05 +0000

    try top fix bug in last if statement

--- run.py
+++ run.py
@@ -9,36 +9,60 @@ def create_board(b_size):
         board.append([0] * b_size)
     return board
 
+print( "Welcome to Battleship!! \n")
+
+
+
 def print_board(board):
     """
     Prints game board to the terminal
     """
     for row in board:
         print(" ".join(row))
+    
+def get_input(prompt, minimum = None):
+    while True:
+        try:
+            value = int(input(prompt))
+            if minimum is not None and value < minimum:
+                print("Input must be at least {}".format(minimum))
+                continue
+            return value
+        except ValueError:
+            print("You must enter a number.")
 
-size = int(input("Enter board size: (Minimum size requirement:5) "))
+size = int(input("Enter board size: (Minimum size requirement: 5) "))
 if size < 5:
-    size == 5
+    size = 5
 
 board = create_board(size)
 battleship_row = randint(0, size - 1)
 battleship_col = randint(0, size - 1)
 
-for turn in range(6):
+for turn in range(0, 5):
     print("Turn", turn + 1)
-    guess_row = int(input("Guess Row(O-" + str(size -1) + "): "))
-    guess_col = int(input("Guess Col(O-" + str(size -1) + "): "))
+    guess_row = get_input("Guess Row(O-" + str(size -1) + "): ")
+    guess_col = get_input("Guess Col(O-" + str(size -1) + "): ")
 
     if guess_row < 0 or guess_row >= size or guess_col < 0 or guess_col >= size:
         print("Oops, you hit land")
         continue
-    if guess_row == ship_row and guess_col == ship_col:
+    elif guess_row == battleship_row and guess_col == battleship_col:
         print("Bullseye!! You sank my Battleship")
+        break
     else:
         print("Missed me!!")
-        board[guess_row][guess_col] == "X":
-    print(board(board))
+    
+    if board[guess_row][guess_col] == "X":
+        print(board(board))
 
-    if turn == 5:
-        print("Game Over")
+        if board[battleship_row][balttleship_col] !=0:
+            print("You're getting warmer")
+        elif board[battleship_row + 1][balttleship_col] !=0 or board[battleship_row - 1][balttleship_col] !=0 or \
+             board[battleship_row + 1][balttleship_col + 1] !=0 or board[battleship_row + 1][balttleship_col -1] !=0:
+             print("You're getting colder")  
+        board[guess_row][guess_col] = "X"    
+
+else:
+    print("Game Over")
     
